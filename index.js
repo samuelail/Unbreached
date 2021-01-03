bbb88fd5c558e743e5a9814743d1409f3cf234af
Samuel <ailemenizzy@gmail.com>
2021-01-03 02:14:26 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/** This function only checks the email accross a database
+to see if there is a record of it being breached **/
+function checkEmail() {
+
+}
+
+/** This function only checks both the email and password 
+ * accross a database  to see if there is a record of it 
+ * being breached **/
+function checkEmailPassword() {
+
+}
